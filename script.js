eb08adcfd8524b879651442daf5813ec71ef0b65
Durban86 <durban86@verizon.net>
2023-02-07 17:54:26 -0500

    updated the del button to prevent textContent being empty

--- script.js
+++ script.js
@@ -10,40 +10,38 @@ let num1 = null;
 let num2 = null;
 let currentOperation = null;
 
-function add(x, y) {
-	return x + y;
-}
+//Instructions called for making these functions, that I commented out. Not sure they're needed or make my solution simpler
 
-function subtract(x, y) {
-	return x - y;
-}
+// function add(x, y) {
+// 	return x + y;
+// }
 
-function multiply(x, y) {
-	return x * y;
-}
+// function subtract(x, y) {
+// 	return x - y;
+// }
 
-function divide(x, y) {
-	return x / y;
-}
+// function multiply(x, y) {
+// 	return x * y;
+// }
 
-function operate(operator, x, y) {
-	if (operator == add) {
-		return add(x, y);
-	} else if (operator == subtract) {
-		return subtract(x, y);
-	} else if (operator == multiply) {
-		return multiply(x, y);
-	} else if (operator == divide) {
-		return divide(x, y);
-	} else {
-		return "Invalid Operator"
-	}
+// function divide(x, y) {
+// 	return x / y;
+// }
+
+// function operate(operator, x, y) {
+// 	if (operator == add) {
+// 		return add(x, y);
+// 	} else if (operator == subtract) {
+// 		return subtract(x, y);
+// 	} else if (operator == multiply) {
+// 		return multiply(x, y);
+// 	} else if (operator == divide) {
+// 		return divide(x, y);
+// 	} else {
+// 		return "Invalid Operator"
+// 	}
 	
-}
-
-// numberBtn.addEventListener('click', (event) => {
-// 	display.textContent += event.target.value;
-// });
+// }
 
 numberBtn.forEach(btn => {
 	btn.onclick = function (btn) {
@@ -60,8 +58,8 @@ numberBtn.forEach(btn => {
 operatorBtn.forEach(btn => {
 	btn.onclick = function (btn) {
 		num1 = Number(display.textContent)
-		display.textContent = ""
 		currentOperation = btn.target.value
+		display.textContent = ""
 
 	}
 });
@@ -92,62 +90,10 @@ clearBtn.addEventListener('click', () => {
 });
 
 delBtn.addEventListener('click', () => {
-	display.textContent = display.textContent.slice(0, -1);
+	if (display.textContent.length <= 1) {
+		display.textContent = "0"
+	} else {
+		display.textContent = display.textContent.slice(0, -1);
+	}
 });
-// zeroBtn.addEventListener('click', () => {
-// 	display.textContent += "0";	
-// });
-// oneBtn.addEventListener('click', () => {
-// 	display.innerText += "1";	
-// });
-// twoBtn.addEventListener('click', () => {
-// 	display.innerText += "2";	
-// });
-// threeBtn.addEventListener('click', () => {
-// 	display.innerText += "3";	
-// });
-// fourBtn.addEventListener('click', () => {
-// 	display.innerText += "4";	
-// });
-// fiveBtn.addEventListener('click', () => {
-// 	display.innerText += "5";	
-// });
-// sixBtn.addEventListener('click', () => {
-// 	display.innerText += "6";	
-// });
-// sevenBtn.addEventListener('click', () => {
-// 	display.innerText += "7";	
-// });
-// eightBtn.addEventListener('click', () => {
-// 	display.innerText += "8";	
-// });
-// nineBtn.addEventListener('click', () => {
-// 	display.innerText += "9";	
-// });
-// percentBtn.addEventListener('click', () => {
-// 	display.innerText = "%";	
-// });
-// plusMinusBtn.addEventListener('click', () => {
-// 	display.innerText = "plus minus";	
-// });
-// divideBtn.addEventListener('click', () => {
-// 	display.innerText = "/";	
-// });
-// multiplyBtn.addEventListener('click', () => {
-// 	display.innerText = "*";	
-// });
-// subtractBtn.addEventListener('click', () => {
-// 	display.innerText = "-";	
-// });
-// addBtn.addEventListener('click', () => {
-// 	display.innerText = "+";	
-// });
-// decimalBtn.addEventListener('click', () => {
-// 	display.innerText = ".";	
-// });
-// equalBtn.addEventListener('click', () => {
-// 	display.innerText = "=";	
-// });
-// clearBtn.addEventListener('click', () => {
-// 	display.innerText = "";	
-// });
+
